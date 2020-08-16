68263730cf3496617d6e7f5557220b5589dec1ed
kdervic1 <kdervic1@etf.unsa.ba>
2020-08-16 22:12:42 +0200

    patient.fxml modification, added controller actions

--- src/ba/unsa/etf/rpr/projekat/Controllers/PatientController.java
+++ src/ba/unsa/etf/rpr/projekat/Controllers/PatientController.java
@@ -4,13 +4,21 @@ import ba.unsa.etf.rpr.projekat.DAO.UserDAO;
 import ba.unsa.etf.rpr.projekat.Models.Doctor;
 import ba.unsa.etf.rpr.projekat.Models.Patient;
 import ba.unsa.etf.rpr.projekat.Models.User;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.text.Text;
 
 public class PatientController {
     private final UserDAO userDAO;
     private Patient patient;
     public Text txtWelcome;
+    public Text txtName;
+    public Text txtEmail;
+    public Text txtPhoneNumber;
+    public Text txtUsername;
+    public Text txtPassword;
+    public CheckBox cbShowPassword;
 
     public PatientController(User user, UserDAO userDAO) {
         patient = new Patient(user.getFirstName(), user.getLastName(), user.getEmail(), user.getPhoneNumber(), user.getUserName(), user.getPassword(), user.getGender(), user.getDateOfBirth());
@@ -24,5 +32,20 @@ public class PatientController {
         } else {
             txtWelcome.setText("Dobrodošao, " + patient.getFirstName());
         }
+        txtName.setText(patient.getFirstName() + " " + patient.getLastName());
+        txtEmail.setText(patient.getEmail());
+        txtPhoneNumber.setText(patient.getPhoneNumber());
+        txtUsername.setText(patient.getUserName());
+        txtPassword.setText(patient.getPassword());
+
+        txtPassword.setVisible(false);
+        cbShowPassword.setSelected(false);
+    }
+
+    public void showPasswordAction(ActionEvent actionEvent){
+        if(cbShowPassword.isSelected()){
+            txtPassword.setVisible(true);
+        }
+        else txtPassword.setVisible(false);
     }
 }
